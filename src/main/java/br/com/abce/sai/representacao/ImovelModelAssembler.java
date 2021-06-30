f5941060c5e242d6264f03b9e55b97971596d91f
Alan Barbosa <alanbarbosa.gyn@gmail.com>
2021-06-29 23:12:26 -0300

    Adicinoando link das fotos.

--- src/main/java/br/com/abce/sai/representacao/ImovelModelAssembler.java
+++ src/main/java/br/com/abce/sai/representacao/ImovelModelAssembler.java
@@ -1,13 +1,19 @@
 package br.com.abce.sai.representacao;
 
+import br.com.abce.sai.controller.FotoController;
 import br.com.abce.sai.controller.ImovelController;
 import br.com.abce.sai.dto.ImovelDto;
 import br.com.abce.sai.persistence.model.Imovel;
+import br.com.abce.sai.persistence.model.ImovelHasFoto;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -23,7 +29,16 @@ public class ImovelModelAssembler implements RepresentationModelAssembler<Imovel
 
     @Override
     public EntityModel<ImovelDto> toModel(Imovel entity) {
+
+        ArrayList builderFotos = new ArrayList<WebMvcLinkBuilder>();
+
+        builderFotos.add(linkTo(methodOn(ImovelController.class).findByOne(entity.getIdImovel())).withSelfRel());
+
+        for (ImovelHasFoto imovelHasFoto : entity.getImovelHasFotosByIdImovel()) {
+            builderFotos.add(linkTo(methodOn(FotoController.class).findByOne(imovelHasFoto.getId().getFotoIdFoto(), null, null)).withRel("fotos"));
+        }
+
         return EntityModel.of(modelMapper.map(entity, ImovelDto.class),
-                linkTo(methodOn(ImovelController.class).findByOne(entity.getIdImovel())).withSelfRel());
+                builderFotos);
     }
 }
